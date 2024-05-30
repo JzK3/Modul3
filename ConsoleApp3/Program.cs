24e76302ec9824731b954d9a9c17a5506ad13976
JzK <andre-kiryushin@mail.ru>
2024-05-31 09:29:54 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Linq;
+
+namespace ConsoleApp3
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Azaz";
+            Console.WriteLine(MyName);
+        }
+    }
+}
